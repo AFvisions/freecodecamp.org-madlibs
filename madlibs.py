820ce0cf26c70b2b59f664441649d6d319ae3daa
AFvisions <andrew@afvisions.com>
2022-01-19 17:56:07 -0600

    FinalizedMadlibs

--- madlibs.py
+++ madlibs.py
@@ -1,9 +1,18 @@
 # string concatenation (aka how to put strings together)
 # suppose we want to create a string that says "subscribe to AFvisions"
-youtuber = "AFvisions" # some string variable
+# youtuber = "AFvisions" # some string variable
 
 # a few ways to do this
-print("subscribe to " + youtuber)
-print("subscrive to {}" .format(youtuber))
-print(f"subscrive to {youtuber}")
+# print("subscribe to " + youtuber)
+# print("subscribe to {}" .format(youtuber))
+# print(f"subscribe to {youtuber}")
 
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all of the time because \
+I love to {verb1}. Stay hyrdated and {verb2} like you are {famous_person}!"
+
+print(madlib)
