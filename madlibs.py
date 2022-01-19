ad7758a1594b5231caff264f74963ba0d0322b28
AFvisions <andrew@afvisions.com>
2022-01-19 16:30:44 -0600

    freeCodecamp.org - Madlibs

---
+++ madlibs.py
@@ -0,0 +1,9 @@
+# string concatenation (aka how to put strings together)
+# suppose we want to create a string that says "subscribe to AFvisions"
+youtuber = "AFvisions" # some string variable
+
+# a few ways to do this
+print("subscribe to " + youtuber)
+print("subscrive to {}" .format(youtuber))
+print(f"subscrive to {youtuber}")
+
